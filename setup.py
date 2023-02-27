71d23cc2adb7ca39d93216f684d64295a96e3708
francesco <info.francescobianco@gmail.com>
2023-02-27 18:58:27 +0100

    Release

---
+++ setup.py
@@ -0,0 +1,42 @@
+import os
+from setuptools import setup, find_packages
+
+from htmlmin import __version__
+
+here = os.path.dirname(__file__)
+
+README = open(os.path.join(here, 'README.md')).read()
+LICENSE = open(os.path.join(here, 'LICENSE')).read()
+
+setup(
+    name='arduino-web-inject',
+    version=__version__,
+    license='MIT',
+    description='Inject and build web files into your sketches',
+    long_description=README,
+    url='https://github.com/fulminati/arduino-web-inject',
+    download_url='https://github.com/fulminati/arduino-web-inject',
+    author='Francesco Bianco',
+    author_email='bianco@javanile.org',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True,
+    test_suite='tests.tests.suite',
+    install_requires=[],
+    tests_require=[],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Topic :: Text Processing :: Markup :: HTML",
+    ],
+    entry_points={
+        'console_scripts': [
+            'arduino-web-inject = arduino-web-inject',
+        ],
+    },
+)
